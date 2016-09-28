461ee452c3f10b67b860ef67425a6ba1269df9df
sherri010 <aminshahidi.sh@gmail.com>
2016-09-28 16:46:10 -0700

    shows rendered data using templates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,30 +2,34 @@ $(document).ready(function(){
   console.log('js is ready!');
   google.maps.event.addDomListener(window, 'load', initialize);
 
+  //preparing handlebar
+  var source = $('#experience-handle-bar').html();
+  var template = Handlebars.compile(source);
+
   $.ajax({
     type: 'GET',
     url: 'api/experiences',
     data: [],
     success: function(data) {
       console.log(data);
+      var content = template({Experience : data});
+      $('#main').append(content);
     }
-  })
+  });
 });//ending ready
 
 
-function initialize() {
+function initialize(){
   var bangalore = { lat: 37.7749, lng: -122.431297 };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 3,
     center: bangalore
   });
-
   // This event listener calls addMarker() when the map is clicked.
   google.maps.event.addListener(map, 'click', function(event) {
     //console.log(event.latLng)
     addMarker(event.latLng, map);
   });
-
   // Add a marker at the center of the map.
   addMarker(bangalore, map);
 }
@@ -34,11 +38,7 @@ function initialize() {
 var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var labelIndex = 0;
 
-// Adds a marker to the map.
 function addMarker(location, map) {
-  // Add the marker at the clicked location, and add the next-available label
-  // from the array of alphabetical characters.
-  //console.log(location)
   var marker = new google.maps.Marker({
     position: location,
     label: labels[labelIndex++ % labels.length],
